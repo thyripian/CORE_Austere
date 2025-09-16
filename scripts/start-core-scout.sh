d48b9a651736a6c0dddd8d70356bba0faa7ab87f
Kevan Christopher White <110140474+thyripian@users.noreply.github.com>
2025-09-16 14:11:07 -0400

    cleanup of outdated components

--- scripts/start-core-scout.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ -f "venv/bin/activate" ]; then
-  source venv/bin/activate
-fi
-echo "[Scout] Launching CORE-Scout..."
-python run_app.py --config config/settings_offline.json
