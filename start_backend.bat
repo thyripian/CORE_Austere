e239b22b313ec3ef09e546d39e2c244e51b6cd81
thyripian <110140474+thyripian@users.noreply.github.com>
2025-09-19 10:23:49 -0400

    Delete start_backend.bat

--- start_backend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set API_PORT=52478
-set DB_PATH=D:\CORE_Scout\military_reports_db.db
-venv\Scripts\python.exe run_app_dynamic.py --db "D:\CORE_Scout\military_reports_db.db" --port 52478
-pause
